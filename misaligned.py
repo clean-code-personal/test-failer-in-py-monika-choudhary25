6b261a122dcc9f5a6e6bc47dcca7640276b2ec78
monika-choudhary25 <monika.choudhary@philips.com>
2024-11-20 15:26:52 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -18,4 +18,7 @@ assert(len(color_map) == 25), "Color map size should be 25"
 assert(all('|' in entry for entry in color_map)), "Separator '|' missing in color map entries"
 assert(all(entry.split('|')[0].strip().isdigit() for entry in color_map)), "Index should be a number"
 assert(color_map[-1] == '24 | Violet | Slate')
+assert(all(entry.find('|') - len(entry.split('|')[0]) == 2 for entry in color_map)), "Index alignment issue"
+assert(all(entry.find('|', entry.find('|') + 1) - entry.find('|') - 1 == 6 for entry in color_map)), "Major color alignment issue"
+assert(all(len(entry.split('|')[2]) == 6 for entry in color_map)), "Minor color alignment issue"
 print("All is well (maybe!)\n")
